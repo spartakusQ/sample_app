d0e237a032923ac631cd92fbfa52766270c634ad
spartakusQ <spartakus1688@gmail.com>
2017-06-30 23:27:54 +0300

    Make form to registration new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
   end
   def new
+    @user = User.new
   end
 end
