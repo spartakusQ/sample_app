868b8e5d6881ff5a9523aefffdfed5a8be1aa1b4
spartakusQ <spartakus1688@gmail.com>
2017-07-01 01:05:34 +0300

    Make redirect & create first user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
-      if @user.save # Обработать успешное сохранение.
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
         render 'new'
     end
