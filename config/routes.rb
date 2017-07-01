187b7816ad0dd708011d27b977b28ea6146e7108
spartakusQ <spartakus1688@gmail.com>
2017-07-01 12:34:43 +0300

    Create session controller & log in menu

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contacts' => 'static_pages#contacts'
   get  'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
