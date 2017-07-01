df7bf7b6546ed4fc06c336d3d124980aba0d8384
spartakusQ <spartakus1688@gmail.com>
2017-07-01 13:20:32 +0300

    Create to log in user menu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,16 @@
 module SessionsHelper
+
   def log_in(user)
-    session[:user_id] = user.id 
+    session[:user_id] = user.id
+  end
+
+  # Возвращает текущего вошедшего пользователя (если есть).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Возвращает true, если пользователь зарегистрирован, иначе возвращает false.
+  def logged_in?
+    !current_user.nil?
   end
 end
