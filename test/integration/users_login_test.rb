75ddc1f08edc6d420ec9b16c234b238c8d843ceb
spartakusQ <spartakus1688@gmail.com>
2017-07-01 21:29:54 +0300

    Fix some bug

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,8 +22,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # Сымитировать щелчок на ссылке для выхода во втором окне.
     delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path, count: 1
-    assert_select "a[href=?]", user_path(@user), count: 1
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
