c2fb3ab55c9307ba52befd3b48e568f86666a0d9
spartakusQ <spartakus1688@gmail.com>
2017-07-04 00:50:29 +0300

    Add to function Edit button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
       if @user.save
@@ -15,7 +18,23 @@ class UsersController < ApplicationController
         render 'new'
     end
   end
+
+  def edit
+    @user = User.find{params[:id]}
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if
+    @user.update_attributes(user_params)
+    # Обработать успешное изменение.
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def user_params
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
