65263ee1958ac0d371dc4703e40c819099e7507a
spartakusQ <spartakus1688@gmail.com>
2017-07-04 18:09:22 +0300

    Make All User page & listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -54,7 +58,7 @@ class UsersController < ApplicationController
     store_location
     flash[:danger] = "Please log in."
     redirect_to login_url
-    end 
+    end
   end
 
   def correct_user
