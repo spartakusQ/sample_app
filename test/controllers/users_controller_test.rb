2a0b885e7189c5244ab72aed382334ddc16cb18b
spartakusQ <spartakus1688@gmail.com>
2017-07-05 23:07:26 +0300

    Test to admin function "delete"

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,6 +17,21 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
   test "should redirect edit when not logged in" do
     get :edit, id: @user
     assert_not flash.empty?
